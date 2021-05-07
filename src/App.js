d570d6a05e98a23d9e44e4984a67171f6a566c1f
Rahul Raj Mogili <mogilirahulraj@gmail.com>
2021-05-07 12:50:33 -0400

    Created background with time periods

--- src/App.js
+++ src/App.js
@@ -1,9 +1,93 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="row">
+        <div className="col"></div>
+      </div>
+      <div className="year-divider">
+          <span className="divider"></span>
+          <span>500 BCE</span>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+      </div>
+      <div className="year-divider">
+          <span className="divider"></span>
+          <span>450 BCE</span>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+      </div>
+      <div className="year-divider">
+          <span className="divider"></span>
+          <span>400 BCE</span>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+      </div>
+      <div className="year-divider">
+          <span className="divider"></span>
+          <span>350 BCE</span>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+      </div>
+      <div className="year-divider">
+          <span className="divider"></span>
+          <span>300 BCE</span>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+      </div>
+      <div className="year-divider">
+          <span className="divider"></span>
+          <span>250 BCE</span>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+      </div>
+      <div className="year-divider">
+          <span className="divider"></span>
+          <span>200 BCE</span>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+      </div>
+      <div className="year-divider">
+          <span className="divider"></span>
+          <span>150 BCE</span>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+      </div>
+      <div className="year-divider">
+          <span className="divider"></span>
+          <span>100 BCE</span>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+      </div>
+      <div className="year-divider">
+          <span className="divider"></span>
+          <span>50 BCE</span>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+      </div>
+      <div className="year-divider">
+          <span className="divider"></span>
+          <span>0</span>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+      </div>
+      <div className="year-divider">
+          <span className="divider"></span>
+          <span>50 CE</span>
+      </div>
     </div>
   );
 }
